461347ee6b05cc08a7bbe7aa4b70e9f92f9ebae3
siva-piskool <siva.piskool@gmail.com>
2019-07-22 20:25:48 +0530

    removed unnecessary files

--- reactApp/src/Test.js
+++
@@ -1,9 +0,0 @@
-import React,{Component} from 'react';
-class Test extends Component{
-    render(){
-        return(<div>
-            <img src={require("./images/Gallery-images/01.jpg")} />
-        </div>);
-    }
-};
-export default Test;
