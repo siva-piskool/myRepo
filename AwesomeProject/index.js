4da039fb89103b4698fb7b329d004644bc12419f
siva-piskool <siva.piskool@gmail.com>
2019-08-31 17:51:28 +0530

    unnecessary files removed

--- AwesomeProject/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-import { AppRegistry } from "react-native";
-import { name as appName } from "./app.json";
-import Inputs from "./Dummy/Inputs";
-
-
-AppRegistry.registerComponent(appName, () => Inputs);
