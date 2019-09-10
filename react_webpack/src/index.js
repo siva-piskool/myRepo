38b659337b795d04f429bc28edd1da1dab5c0934
siva-piskool <siva.piskool@gmail.com>
2019-09-10 16:09:57 +0530

    reactapp created using webpack

---
+++ react_webpack/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+
+const App = () => {
+  return <div>
+  <p>Hello React,Webpack 4 & Babel 7!</p>
+  </div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
