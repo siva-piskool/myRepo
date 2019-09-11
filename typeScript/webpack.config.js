bb7aef07cad67a1d31f1a540468cf72bce1265ca
siva-piskool <siva.piskool@gmail.com>
2019-09-11 11:17:47 +0530

    adding typescript to react completed successfully

--- typeScript/webpack.config.js
+++ typeScript/webpack.config.js
@@ -1,12 +1,9 @@
 module.exports = {
     mode: "production",
-
-    // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx"]
+       extensions: [".ts", ".tsx"]
     },
 
     module: {
@@ -20,7 +17,6 @@ module.exports = {
                     }
                 ]
             },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
                 enforce: "pre",
                 test: /\.js$/,
@@ -28,11 +24,6 @@ module.exports = {
             }
         ]
     },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
         "react": "React",
         "react-dom": "ReactDOM"
