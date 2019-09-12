7450b24c0e8cf4b167f3f027c5f2189acb81c4da
siva-piskool <siva.piskool@gmail.com>
2019-09-12 17:57:44 +0530

    Reactapp with 3 webpack files(based on mode) updated

--- typeScript/src/components/Hello.tsx
+++ typeScript/src/components/Hello.tsx
@@ -3,6 +3,6 @@ import * as React from "react";
 export interface HelloProps { compiler: string; framework: string; name:string; }
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}! by {this.props.name}</h1>;
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}! by {this.props.name} </h1>;
     }
 }
--- typeScript/src/index.tsx
+++ typeScript/src/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from "react-dom";
 import { Hello } from "./components/Hello";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" name ="siva" />,
+    <Hello compiler="TypeScript" framework="React" name ="kk" />,
     document.getElementById("example")
 );
