f98c21c4d8e46b8854d2ae624acb6b68f42b8d47
siva-piskool <siva.piskool@gmail.com>
2019-09-13 17:21:05 +0530

    HtmlWebpackPlugin added to webapck.config

--- typeScript/src/index.tsx
+++ typeScript/src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+
 import { Hello } from "./components/Hello";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" name ="SivaKumar" />,
+    <Hello compiler="TypeScript" framework="React" name ="" />,
     document.getElementById("example")
 );
