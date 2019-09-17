06b708b4ee761d312eda2435b09c0ee9fa586c71
siva-piskool <siva.piskool@gmail.com>
2019-09-17 11:01:04 +0530

    Just for testing

---
+++ react-app/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+const App = () => {
+  return <div><p>Hello React,Webpack 4  Babel 7!</p></div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
--- react_webpack (modes)/webpack.dev.js
+++ react_webpack (modes)/webpack.dev.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common.js');
 module.exports = merge(common,{
     mode:'development',
     devtool:'inline-sourece-map',
-    devServer:{
+    devserver:{
         contentBase:'./dist'
     }
 });
