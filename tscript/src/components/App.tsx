3998ccdac2d23199761ac32b162340fcd05e8412
siva-piskool <siva.piskool@gmail.com>
2019-10-03 15:05:13 +0530

    New reactapp created with name tscript and added typescript to it

---
+++ tscript/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/index.css';
+class App extends React.PureComponent {
+	render() {
+		return (
+			<div>
+				<h1>Hello World!Created by me</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
