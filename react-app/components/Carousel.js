ae4eb2bad4a56739ba2007152dd62c8d2d2ee4bd
siva-piskool <siva.piskool@gmail.com>
2019-10-14 15:08:37 +0530

    Carousel with pure-react-carousel updated

--- react-app/components/Carousel.js
+++ react-app/components/Carousel.js
@@ -7,11 +7,11 @@ class Carousel extends Component{
         return(<CarouselProvider naturalSlideWidth={100} naturalSlideHeight={125} totalSlides={3} isPlaying={true}  >
         <Slider>
             <Slide index={0}><img src={require("./images/carousel-0.jpg")} />
-           <DotGroup dotNumbers={true}></DotGroup> </Slide>
+           <DotGroup disableActiveDots={true} className="carousel_dot-group" ></DotGroup> </Slide>
             <Slide index={1} ><img src={require("./images/carousel-1.jpg")}/>
-            <DotGroup dotNumbers={true}></DotGroup></Slide>
+            <DotGroup disableActiveDots={true} className="carousel_dot-group"></DotGroup></Slide>
             <Slide index={2} ><img src={require("./images/carousel-2.jpg")} />
-            <DotGroup dotNumbers={true}></DotGroup></Slide>
+            <DotGroup disableActiveDots={true} className="carousel_dot-group"></DotGroup></Slide>
         </Slider>
     </CarouselProvider>)
     }
