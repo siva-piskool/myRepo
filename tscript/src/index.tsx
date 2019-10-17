aa86993dabf023c16e523fdca6aa09a64581f590
siva-piskool <siva.piskool@gmail.com>
2019-10-17 17:40:39 +0530

    common Address component added

--- tscript/src/index.tsx
+++ tscript/src/index.tsx
@@ -5,6 +5,5 @@ import 'jquery';
 import "popper.js";
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './App.css';
-import Test from './components/Demo/Test';
-
-ReactDOM.render(<Test msg="Hello"/>, document.getElementById('root'));
+import Address from './common/Address';
+ReactDOM.render(<Address/>, document.getElementById('root'));
