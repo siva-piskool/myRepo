11562df5a6a48f8f340180c460e6b82101b84826
siva-piskool <siva.piskool@gmail.com>
2019-10-21 14:55:22 +0530

    Accessing state props from other functions using hooks

--- react-app/components/Hooks/Exp1.js
+++ react-app/components/Hooks/Exp1.js
@@ -2,17 +2,11 @@ import React, {useState,useEffect} from 'react';
 export default function Exp1(){
     const [name,setName]=useState("Mahi");
     const [surname,setsurName]= useState('Prince');
+    const width = useWindowWidth();
     useEffect(()=>{
         document.title= name + " "+surname;
     })
-    const [width, setWidth]=useState(window.innerWidth);
-    useEffect(()=>{
-        const handleResize =()=>setWidth(window.innerWidth);
-        window.addEventListener("resize",handleResize);
-        return()=>{}
-
-        }
-    )
+    
     function handleNameChange(e){
         setName(e.target.value);
     }
@@ -33,4 +27,15 @@ export default function Exp1(){
            <p>{width}</p>
         </section>
     </div>)
+}
+function useWindowWidth(){
+    const [width, setWidth]=useState(window.innerWidth);
+    useEffect(()=>{
+        const handleResize =()=>setWidth(window.innerWidth);
+        window.addEventListener("resize",handleResize);
+        return()=>{}
+
+        }
+    )
+    return width;
 }
