9bdc655f0097b5f786fa09b315fdbbaafb9ff63e
siva-piskool <siva.piskool@gmail.com>
2019-10-21 11:17:17 +0530

    a state with 2 poperties using hooks added

--- react-app/components/Hooks/Exp1.js
+++ react-app/components/Hooks/Exp1.js
@@ -1,12 +1,21 @@
 import React, {useState} from 'react';
 export default function Exp1(){
     const [name,setName]=useState("Mahi");
+    const [surname,setsurName]= useState('Prince');
     function handleNameChange(e){
         setName(e.target.value);
     }
+    function handlesurNameChange(e){
+        setsurName(e.target.value);
+    }
     return(<div>
         <section>
+            <label>Name:</label>
             <input value={name} onChange={handleNameChange} />
+        </section>
+        <section>
+            <label>SurName:</label>
+            <input value={surname} onChange={handlesurNameChange} />
         </section>
     </div>)
 }
