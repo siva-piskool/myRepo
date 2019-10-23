6f7b243bd354b745454ef69728aa7609fa6b3393
siva-piskool <siva.piskool@gmail.com>
2019-10-23 15:46:45 +0530

    Unneccessary files removed

--- typeScript/src/components/Hello.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-export interface HelloProps { compiler: string; framework: string; name:string; }
-export class Hello extends React.Component<HelloProps, {}> {
-    render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}! by {this.props.name} </h1>;
-    }
-}
