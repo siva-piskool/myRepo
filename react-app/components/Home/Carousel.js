04fc335d80d15553e36ed312e7d40a5bad710409
siva-piskool <siva.piskool@gmail.com>
2019-10-24 18:48:14 +0530

    Carousel updated as Dynamic

--- react-app/components/Home/Carousel.js
+++ react-app/components/Home/Carousel.js
@@ -1,44 +1,34 @@
-import React, { useState } from 'react';
-import './Carousel.css';
-import {Carousel} from 'react-bootstrap';
-export default function HomeCarousel(){
-    const[index,setIndex]=useState(0);
-    const[direction,setDirection]=useState(0);
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-        setDirection(e.direction);
-      };
-    
-    return(<Carousel activeIndex={index} direction={direction} onSelect={handleSelect}>
-<Carousel.Item>
-    <img className="d-block w-100" src={require('../../images/carousel-0.jpg')} />
-   <Carousel.Caption>
-   Study, because they can never take it away from you.
-    </Carousel.Caption>
-</Carousel.Item>
-<Carousel.Item>
-    <img className="d-block w-100" src={require('../../images/carousel-1.jpg')} />
-    <Carousel.Caption>
-    Science! if you have the conditions, you get the result.
-    </Carousel.Caption>
-</Carousel.Item>
-<Carousel.Item>
-    <img className="d-block w-100" src={require('../../images/carousel-2.jpg')} />
-    <Carousel.Caption>
-    We create light through knowledge, students will find the way.
-    </Carousel.Caption>
-</Carousel.Item>
-<Carousel.Item>
-    <img className="d-block w-100" src={require('../../images/carousel-3.jpg')} />
-    <Carousel.Caption>
-    We provide what students will like!
-    </Carousel.Caption>
-</Carousel.Item>
-<Carousel.Item>
-    <img className="d-block w-100" src={require('../../images/carousel-4.jpg')} />
-    <Carousel.Caption>
-    We develop a passion for learning.
-    </Carousel.Caption>
-</Carousel.Item>
-    </Carousel>)
+import React, { useState, useEffect } from "react";
+import "./Carousel.css";
+import { Carousel } from "react-bootstrap";
+export default function HomeCarousel() {
+  const [index, setIndex] = useState(0);
+  const [direction, setDirection] = useState(0);
+  const [carousel, setCarousel] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/carousel")
+      .then(response => response.json())
+      .then(res => {
+        setCarousel(res);
+      });
+  });
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+    setDirection(e.direction);
+  };
+  return (
+    <Carousel activeIndex={index} direction={direction} onSelect={handleSelect}>
+    {carousel.map(img=>(
+        <Carousel.Item key={img.id} >
+        <img
+          className="d-block w-100"
+          src={require(`../../images/carousel-${img.pic}.jpg`)}
+        />
+        <Carousel.Caption>
+        {img.caption}
+        </Carousel.Caption>
+      </Carousel.Item>
+    ))}
+     </Carousel>
+  );
 }
